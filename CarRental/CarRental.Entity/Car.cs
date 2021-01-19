1062b948669ed31638421ac0af2c924cc20c86ae
Hayri Rıza Çimen <hayririzacimen@gmail.com>
2021-01-19 23:25:36 +0300

    Proje dosyası ekle.

---
+++ CarRental/CarRental.Entity/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entity
+{
+    [Table("Car")]
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string CarName { get; set; }
+        public string CarModel { get; set; }
+        public int RentFeeDaily { get; set; }
+        public int CarTotalKM{ get; set; }
+        public int CarTotalSeats { get; set; }
+        public int isRent { get; set; }
+
+        public int CompanyID { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ CarRental/CarRental/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("CarRental")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarRental")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("949689fd-71ae-4a72-8a0d-a5865f219662")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
