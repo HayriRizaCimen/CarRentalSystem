8e2a5bff2fc6a6ceebcb6c684ab20d6b3ed691b1
Hayri Rıza Çimen <hayririzacimen@gmail.com>
2021-02-07 21:47:57 +0300

    Controllers

---
+++ CarRental/CarRental.AspNetWeb.Pure/Controllers/CarController.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Car = CarRental.Entity.Concretes.Car;
+using CarRental.BusinessLogic.Concretes;
+
+namespace CarRental.AspNetWeb.Pure.Controllers
+{
+    public class CarController : Controller
+    {
+
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Car/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(SelectCarByID(id));
+        }
+
+
+        // GET: Car/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Car/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection collection)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                if (InsertCar(collection["CarName"], collection["CarModel"], int.Parse(collection["RentFeeDaily"]), int.Parse(collection["CarTotalKM"]), int.Parse(collection["CarTotalSeats"]), int.Parse(collection["CompanyID"]) ))
+                    return RedirectToAction("ListAll");
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("AspNetWeb.Pure::CarController::InsertCar::Error occured.", ex);
+                //return View();
+            }
+        }
+
+
+
+        // GET: Car/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                return View(SelectCarByID(id));
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        // POST: Car/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                if (UpdateCar(id, collection["CarName"], collection["CarModel"], int.Parse(collection["RentFeeDaily"]), int.Parse(collection["CarTotalKM"]), int.Parse(collection["CarTotalSeats"]), int.Parse(collection["CompanyID"]))
+                    return RedirectToAction("Index");
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Car/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                if (DeleteCar(id))
+                    return RedirectToAction("ListAll");
+                return RedirectToAction("ListAll");
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Operation failed!", ex);
+            }
+        }
+
+        public ActionResult ListAll()
+        {
+            try
+            {
+                IList<Car> cars = ListAllCars().ToList();
+                return View(cars);
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        #region PRIVATE METHODS
+
+        private bool InsertCar(string name, string surname, int number, string email)
+        {
+            try
+            {
+                using (var carBussines = new CarBusiness())
+                {
+                    return carBussines.InsertCar(new Car()
+                    {
+                        CarName = name,
+                        CarSurname = surname,
+                        PhoneNumber = number,
+                        Email = email
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        private bool UpdateCar(int id, string name, string surname, int number, string email)
+        {
+            try
+            {
+                using (var carBussines = new CarBusiness())
+                {
+                    return carBussines.UpdateCar(new Car()
+                    {
+                        CarID = id,
+                        CarName = name,
+                        CarSurname = surname,
+                        PhoneNumber = number,
+                        Email = email
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        private bool DeleteCar(int ID)
+        {
+            try
+            {
+                using (var carBussines = new CarBusiness())
+                {
+                    return carBussines.DeleteCarById(ID);
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        private List<Car> ListAllCars()
+        {
+            try
+            {
+                using (var carBussines = new CarBusiness())
+                {
+                    List<Car> cars = carBussines.SelectAllCars();
+                    return cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        private Car SelectCarByID(int ID)
+        {
+            try
+            {
+                using (var carBussines = new CarBusiness())
+                {
+                    return carBussines.SelectCarById(ID);
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Car doesn't exists.", ex);
+            }
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CarRental/CarRental.AspNetWeb.Pure/Controllers/CustomerController.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Customer = CarRental.Entity.Concretes.Customer;
+using CarRental.BusinessLogic.Concretes;
+
+namespace CarRental.AspNetWeb.Pure.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(SelectCustomerByID(id));
+        }
+
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection collection)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                if (InsertCustomer(collection["CustomerName"], collection["CustomerSurname"], int.Parse(collection["PhoneNumber"]), collection["Email"]))
+                    return RedirectToAction("ListAll");
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("AspNetWeb.Pure::CustomerController::InsertCustomer::Error occured.", ex);
+                //return View();
+            }
+        }
+
+
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                return View(SelectCustomerByID(id));
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                if (UpdateCustomer(id, collection["CustomerName"], collection["CustomerSurname"], int.Parse(collection["PhoneNumber"]), collection["Email"]))
+                    return RedirectToAction("Index");
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                if (DeleteCustomer(id))
+                    return RedirectToAction("ListAll");
+                return RedirectToAction("ListAll");
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Operation failed!", ex);
+            }
+        }
+
+        public ActionResult ListAll()
+        {
+            try
+            {
+                IList<Customer> customers = ListAllCustomers().ToList();
+                return View(customers);
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        #region PRIVATE METHODS
+
+        private bool InsertCustomer(string name, string surname, int number, string email)
+        {
+            try
+            {
+                using (var customerBussines = new CustomerBusiness())
+                {
+                    return customerBussines.InsertCustomer(new Customer()
+                    {
+                        CustomerName = name,
+                        CustomerSurname = surname,
+                        PhoneNumber = number,
+                        Email = email
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        private bool UpdateCustomer(int id, string name, string surname, int number, string email)
+        {
+            try
+            {
+                using (var customerBussines = new CustomerBusiness())
+                {
+                    return customerBussines.UpdateCustomer(new Customer()
+                    {
+                        CustomerID = id,
+                        CustomerName = name,
+                        CustomerSurname = surname,
+                        PhoneNumber = number,
+                        Email = email
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        private bool DeleteCustomer(int ID)
+        {
+            try
+            {
+                using (var customerBussines = new CustomerBusiness())
+                {
+                    return customerBussines.DeleteCustomerById(ID);
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        private List<Customer> ListAllCustomers()
+        {
+            try
+            {
+                using (var customerBussines = new CustomerBusiness())
+                {
+                    List<Customer> customers = customerBussines.SelectAllCustomers();
+                    return customers;
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        private Customer SelectCustomerByID(int ID)
+        {
+            try
+            {
+                using (var customerBussines = new CustomerBusiness())
+                {
+                    return customerBussines.SelectCustomerById(ID);
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogHelper.Log(LogTarget.File, ExceptionHelper.ExceptionToString(ex), true);
+                throw new Exception("Customer doesn't exists.", ex);
+            }
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
